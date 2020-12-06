efde13211416b3dde6af3e53bbaea94b7fd8dadf
connection579 <wlswn4118@naver.com>
2020-12-06 20:34:41 +0900

    숫자 맟추기 게임 연습

---
+++ src/guessGame/GuessGame.java
@@ -0,0 +1,31 @@
+package guessGame;
+import java.util.Scanner;
+public class GuessGame {
+
+	public static void main(String[] args) {
+		int secretNum;
+		int guess;
+		boolean correct = false;
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("GIVE ME SECRET NUMBER:");
+		secretNum = keyboard.nextInt();
+		
+		while(!correct) {
+			System.out.println("GUESS:");
+			guess = keyboard.nextInt();
+			
+			if(guess == secretNum) {
+				correct= true;
+				System.out.println("You are right!");
+			}else if(guess<secretNum) {
+				System.out.println("HIGHR");				
+			}else if(guess>secretNum) {
+				System.out.println("LOWER");
+			}
+		}
+
+	}
+
+}
